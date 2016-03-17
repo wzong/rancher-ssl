87fdb705a2f751f7c7a9ec5b4df937ec730df52f
Marcus Andersson <systemvetaren@gmail.com>
2016-03-17 16:18:03 +0100

    fixed sed command issue.

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ if [ ! -d /etc/letsencrypt/live/$DOMAIN ]; then
   /opt/letsencrypt/letsencrypt-auto certonly --non-interactive --agree-tos --email $EMAIL --standalone -d $DOMAIN
 fi
 
-sed "s/<DOMAIN>/$DOMAIN/" > /etc/nginx/nginx.conf
+sed "s/<DOMAIN>/$DOMAIN/" ./nginx.conf > /etc/nginx/nginx.conf
 nginx -g "daemon off; error_log /dev/stderr notice;"
